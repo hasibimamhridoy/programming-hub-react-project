e958ae2b4b8ee77c8486290ea20da0ca563060ea
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-03-31 17:55:45 +0600

    Purpose of useEffect other than fetching data answered

--- src/components/QuestionAndAnswer/QuestionsAndAnswer.jsx
+++ src/components/QuestionAndAnswer/QuestionsAndAnswer.jsx
@@ -8,13 +8,14 @@ const QuestionsAndAnswer = () => {
       </h1>
 
       <div className="props-vs-state">
+      <h1 className="text-lg lg:text-3xl text-center font-bold my-10">Props Vs State</h1>
         <div>
           <img
             className="w-full lg:h-96 h-72"
             src="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*gCfSTMaxWyM_uiej4QGKeg.jpeg"
             alt=""
           />
-          <h1 className="text-lg lg:text-3xl my-5">Props Vs State</h1>
+        
 
           <p>
             In React, both props and state are used to manage the data that is
@@ -69,13 +70,11 @@ const QuestionsAndAnswer = () => {
           On the Other hand,state is used to manage data within a component. It
           is
           <span className="bg-green-500 lg:p-1 rounded-lg text-white font-bold">
-            
             mutable
           </span>
           and can be modified by the component itself. When the state of a
           component changes,
           <span className="bg-green-500 lg:p-1 rounded-lg text-white font-bold">
-            
             React will re-render the component to update the view
           </span>
           <h1 className="lg:text-2xl mt-5">Example State:</h1>
@@ -83,7 +82,6 @@ const QuestionsAndAnswer = () => {
         </p>
 
         <h1 className="lg:text-2xl mt-5">
-         
           Here are some key differences between props and state:
         </h1>
 
@@ -100,13 +98,14 @@ const QuestionsAndAnswer = () => {
       </div>
 
       <div className="Use-state">
+      <h1 className="text-lg lg:text-3xl my-10 text-center font-bold">How does useState work?</h1>
         <div>
           <img
             className="w-full lg:h-96 h-72"
             src="https://daveceddia.com/images/useState-hook.png"
             alt=""
           />
-          <h1 className="text-lg lg:text-3xl my-5">How does useState work?</h1>
+    
 
           <p>
             In React, the useState hook is a built-in function that allows
@@ -130,34 +129,78 @@ const QuestionsAndAnswer = () => {
               Internally, the useState hook in React uses a concept called
               "closure" to manage state.
             </p>
-
             <h1 className="text-lg lg:text-3xl my-5">
               Example how useState Internally work.
             </h1>
-
             <p>
-              First of all react create a useState function. And this fuction take a initialState value. Like useState(0). and then they decliar e variable like state.and set the state value == initialState. So the state value is now initial value.
-
-              <br /><br />
-
-              and react also declare  a setState function. And setState function get the newState peramiter. and aslo set the state == newState peramiter and re-render it.
-
+              First of all react create a useState function. And this fuction
+              take a initialState value. Like useState(0). and then they decliar
+              e variable like state.and set the state value == initialState. So
+              the state value is now initial value.
+              <br />
+              <br />
+              and react also declare a setState function. And setState function
+              get the newState peramiter. and aslo set the state == newState
+              peramiter and re-render it.
               <br />
             </p>
-            <img className="h-96" src="https://media.graphassets.com/AyJLadrgSFGtKmaeZuvU" alt="" />
+            <img
+              className="h-96"
+              src="https://media.graphassets.com/AyJLadrgSFGtKmaeZuvU"
+              alt=""
+            />
           </p>
 
           <br />
-
-          
-         
-          
         </div>
       </div>
 
+      <div className="useEffect">
+        <div>
+          <h1 className="text-lg lg:text-3xl my-10 text-center font-bold">
+            Purpose of useEffect other than fetching data.?
+          </h1>
 
-      <div className="">
+          <img
+            className="w-full lg:h-96 h-72"
+            src="https://daveceddia.com/images/useEffect-hook.png"
+            alt=""
+          />
 
+          <p className="mt-5">
+            The useEffect hook in React is primarily used for fetching data from
+            APIs or updating the DOM after a component renders. However, it has
+            other use cases as well.
+          </p>
+
+         <div className="space-y-4 mt-4">
+         <p>
+            1. Whenever we need to make use of side effects in our application,
+            useEffect is the way to go.
+          </p>
+
+          <p>
+            2. We can use useEffect to add event listeners to the window or
+            document objects.
+          </p>
+
+          <p>
+            3. We can use useEffect to set up timers that trigger a function
+            after a certain amount of time has elapsed.
+          </p>
+          <p>
+            4. We can use useEffect to read or write data to local storage.
+            Suppose we can show the data if the user close the browser and again visit our site.
+            In this case useEffect role is very important. We can easyli deploy the functionality useing useEffect.
+          </p>
+
+          <p>
+            5. On the most important role in useEffect is Suppose we need the re-render the useEffect when the specific state change or any value change . we easily we can declare it useEffect dependency. Like we have a state and we want if the state value has change then the page re-render so user can display the dynamic value
+          </p>
+         </div>
+
+          <br />
+        </div>
       </div>
     </div>
   );
